06170f19ad379e514304e7c724f9c2120dc140a6
Sergej Garkusha <Tenebrann@googlemail.com>
2023-06-11 16:01:07 +0200

    create form add product

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@ import { Modal, useModal } from 'usemodal-vue3'
 
 const products = ref({})
 const localStorageProducts = JSON.parse(localStorage.getItem('products'))
-console.log(localStorageProducts)
 if (!localStorageProducts) {
   onBeforeMount(async () => {
     products.value = (await axios.get(`/products`)).data
@@ -17,7 +16,9 @@ if (!localStorageProducts) {
 } else {
   products.value = localStorageProducts
 }
-const submitAddProduct = () => {
+const submitAddProduct = (newProduct) => {
+  products.value.push(newProduct)
+  localStorage.setItem('products', JSON.stringify(localStorageProducts))
   setModal('m1', false)
   console.log('submit')
 }
@@ -42,6 +43,7 @@ let setModal = useModal({
   m1: 2,
   m2: 1
 })
+
 modalVisible = setModal('m1', false)
 modalVisible2 = setModal('m2', false)
 </script>
@@ -55,23 +57,7 @@ modalVisible2 = setModal('m2', false)
   ></my-header>
   <input type="search" placeholder="Поиск продуктов..." v-model="query" />
   <product-list :products="queryProducts"></product-list>
-  <Modal
-    name="m1"
-    v-model:visible="modalVisible"
-    :animation="true"
-    :draggable="true"
-    title="Форма Добавление Товара"
-    :okButton="{
-      onclick: () => submitAddProduct(),
-      loading: true,
-      text: 'Купить'
-    }"
-    :cancelButton="{
-      text: 'Отмена'
-    }"
-    ><add-product :submitAddProduct="submitAddProduct"></add-product>
-  </Modal>
-  <div id="modals"></div>
+  <add-product :submitAddProduct="submitAddProduct" :modalVisible="modalVisible"></add-product>
   <Modal name="m2" v-model:visible="modalVisible2"> </Modal>
 </template>
 
--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,22 +1,60 @@
+<!-- eslint-disable vue/no-mutating-props -->
 <template>
-  <form class="container pt-5 pd-5" @submit.prevent.stop="submitAddProduct">
-    <div>Категория</div>
-    <select v-model="selected">
-      <option disabled value="">Категория</option>
-      <option>men's clothing</option>
-      <option>jewelery</option>
-      <option>women's clothing</option>
-      <option>electronic</option>
-    </select>
-    <input type="text" placeholder="Имя товара" id="addTitle" />
-    <input type="text" placeholder="Описание товара" id="addSubtitle" />
-    <input type="text" placeholder="Картинка Url" id="addImg" />
-    <input type="text" placeholder="Цена" id="addPrice" />
-    <input type="text" placeholder="Количество шт." id="addCount" />
-  </form>
+  <Modal
+    name="m1"
+    v-model:visible="props.modalVisible"
+    :animation="true"
+    :draggable="true"
+    title="Форма Добавление Товара"
+    :okButton="{
+      onclick: () => props.submitAddProduct(review),
+      loading: true,
+      text: 'Купить'
+    }"
+    :cancelButton="{
+      text: 'Отмена'
+    }"
+  >
+    <form class="container pt-5 pd-5" @submit.prevent.stop="submitAddProduct">
+      <div>Категория</div>
+      <select v-model="review.category">
+        <option disabled>Категория</option>
+        <option>men's clothing</option>
+        <option>jewelery</option>
+        <option>women's clothing</option>
+        <option>electronic</option>
+      </select>
+      <input type="text" placeholder="Имя товара" id="addTitle" v-model="review.title" />
+      <input
+        type="text"
+        placeholder="Описание товара"
+        id="addSubtitle"
+        v-model="review.description"
+      />
+      <input type="text" placeholder="Картинка Url" id="addImg" v-model="review.image" />
+      <input type="text" placeholder="Цена" id="addPrice" v-model="review.price" />
+      <input type="text" placeholder="Количество шт." id="addCount" v-model="review.rating.count" />
+    </form>
+  </Modal>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+import { v4 } from 'uuid'
+import { Modal } from 'usemodal-vue3'
+
+const props = defineProps(['submitAddProduct', 'modalVisible'])
+
+const review = reactive({
+  category: '',
+  description: '',
+  id: v4(),
+  image: '',
+  price: null,
+  rating: { rate: 0, count: null },
+  title: ''
+})
+</script>
 
 <style lang="scss" scoped>
 input {
